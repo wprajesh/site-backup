480200c1c408f7570558548a9530ba266cb761f6
wprajesh <rajeshit0610@gmail.com>
2015-11-30 13:27:15 -0500

    before zpanel from budgetvm

---
+++ html/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href='https://fonts.googleapis.com/css?family=Merienda' rel='stylesheet' type='text/css'>
+<style>
+body{
+	margin:0 !important;
+    font-family: 'Merienda', cursive;
+}
+#outer{
+	position:fixed;
+	width:100%;
+	height:100%;
+	text-align:center;
+	color:white; 
+	display:table;
+	background:url('images/contruction.jpg');
+	background-size:cover;
+}
+#inner{
+	position:fixed;
+	width:100%;
+	height:100%;
+	text-align:center;
+	color:white;
+	display:table;
+	background:rgba(0,0,0,0.75);
+}
+</style>
+</head>
+<body>
+<div id="outer">
+<div id="inner">
+<div style="display:table-cell;vertical-align:middle;">
+<img src="images/wp-town-center.png" />
+<h2>WPTOWNCENTER is under construction</h2>
+<h4>Email: <a href="mailto:contact@wptowncenter.com" style="color:#ffffff !important;">contact@wptowncenter.com</a></h4>
+
+</div>
+</div>
+</div>
+    
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-68986266-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!--Clicky Code -->
+<a title="Real Time Analytics" href="http://clicky.com/100887045"><img alt="Real Time Analytics" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+<script src="//static.getclicky.com/js" type="text/javascript"></script>
+<script type="text/javascript">try{ clicky.init(100887045); }catch(e){}</script>
+
+</body>
+</html>
---
+++ html/mail-check.php
@@ -0,0 +1,4 @@
+<?php 
+echo phpinfo();
+mail('rajeshit0610@gmail.com','sub','test from php');
+?>
